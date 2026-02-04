9ed9fce035b4cce9a218624cfd9f45d6bc4c094c
Preda <predaomfg@gmail.com>
2026-02-04 23:44:53 +0100

    feat: Introduce mouse interaction and speed-based particle coloring

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ const WINDOW_HEIGHT: u32 = 900;
 const MIN_WINDOW_WIDTH: u32 = 1000;
 const MIN_WINDOW_HEIGHT: u32 = 900;
 
-const PARTICLE_SPACING: f32 = 20.0;
-const NUM_PARTICLES_X: i32 = 20;
-const NUM_PARTICLES_Y: i32 = 10;
+const PARTICLE_SPACING: f32 = 15.0;
+const NUM_PARTICLES_X: i32 = 40;
+const NUM_PARTICLES_Y: i32 = 40;
 const START_X: f32 = -200.0;
 const START_Y: f32 = 200.0;
 
@@ -71,9 +71,36 @@ fn update(app: &App, model: &mut Model, update: Update) {
     let dt = update.since_last.as_secs_f32();
     let win = app.window_rect();
 
+    // Mouse interaction
+    let mouse_pos = app.mouse.position();
+    let is_left_down = app.mouse.buttons.left().is_down();
+    let is_right_down = app.mouse.buttons.right().is_down();
+
+    // Radius of influence
+    let interaction_radius = 200.0;
+    let force_strength = 3000.0;
+
     for particle in model.particle.iter_mut() {
-        // Force integration
-        particle.velocity += vec2(0.0, GRAVITY_Y) * dt;
+        //
+        let mut total_force = vec2(0.0, GRAVITY_Y);
+
+        if is_left_down || is_right_down {
+            let diff = mouse_pos - particle.position;
+            let distance = diff.length();
+
+            // Mouse scope
+            if distance < interaction_radius {
+                let direction_normalized = diff / distance;
+
+                let direction_sign = if is_left_down { -1.0 } else { 1.0 };
+                let interaction_force = force_strength * direction_sign * direction_normalized;
+
+                total_force += interaction_force;
+            }
+        }
+
+        // External Forces Integration
+        particle.velocity += total_force * dt;
 
         // Velocity integration
         particle.position += particle.velocity * dt;
@@ -105,8 +132,12 @@ fn view(app: &App, model: &Model, frame: Frame) {
     draw.background().color(BLACK);
 
     for particle in model.particle.iter() {
+        let speed = particle.velocity.length();
+
+        // 0.4 -> Blue (Low Speed), 0.0 -> Red (High Speed)
+        let color = map_range(speed, 0.0, 800.0, 0.4, 0.0);
         draw.ellipse()
-            .color(DARKGREY)
+            .hsla(color, 0.8, 0.5, 1.0)
             .w_h(PARTICLE_DIAMETER, PARTICLE_DIAMETER)
             .xy(particle.position);
     }
