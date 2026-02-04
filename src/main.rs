b70515417d8a46b3be420dabc4da40792aa524d1
Preda <predaomfg@gmail.com>
2026-02-04 21:58:01 +0100

    feat: Implement multi-particle simulation with a grid of particles and add an FPS display.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ struct Particle {
 
 struct Model {
     _window: window::Id,
-    particle: Particle,
+    particle: Vec<Particle>,
 }
 
 fn main() {
@@ -25,39 +25,72 @@ fn model(app: &App) -> Model {
         .build()
         .unwrap();
 
-    let initial_particle = Particle {
-        position: vec2(0.0, 0.0), // (0,0) is center of screen
-        velocity: vec2(5.0, 2.5),
-    };
+    let mut particles = Vec::new();
+    let spacing = 20.0;
+    let num_x = 20;
+    let num_y = 20;
+    // Total: 400 particles
+
+    let start_x = -200.0;
+    let start_y = 200.0;
+
+    for i in 0..num_x {
+        for j in 0..num_y {
+            let x = start_x + (i as f32 * spacing);
+            let y = start_y - (j as f32 * spacing);
+
+            let p = Particle {
+                position: vec2(x, y),
+                velocity: vec2(random_range(-1.0, 1.0), random_range(-1.0, 1.0)),
+            };
+
+            particles.push(p);
+        }
+    }
+
+    println!("{} particles", particles.len());
+
     Model {
         _window,
-        particle: initial_particle,
+        particle: particles,
     }
 }
 
 fn update(app: &App, model: &mut Model, _update: Update) {
-    model.particle.position += model.particle.velocity;
+    for particle in model.particle.iter_mut() {
+        particle.position += particle.velocity;
+    }
 
     let win = app.window_rect();
 
-    if model.particle.position.x > win.right() || model.particle.position.x < win.left() {
-        model.particle.velocity.x *= -1.0; // Reflect the particle
-    }
+    for particle in model.particle.iter_mut() {
+        if particle.position.x > win.right() || particle.position.x < win.left() {
+            particle.velocity.x *= -1.0; // Reflect the particle
+        }
 
-    if model.particle.position.y > win.top() || model.particle.position.y < win.bottom() {
-        model.particle.velocity.y *= -1.0; // Reflect the particle
+        if particle.position.y > win.top() || particle.position.y < win.bottom() {
+            particle.velocity.y *= -1.0; // Reflect the particle
+        }
     }
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
+    let win = app.window_rect();
 
     draw.background().color(BLACK);
 
-    draw.ellipse()
-        .color(DARKGREY)
-        .w_h(20.0, 20.0)
-        .xy(model.particle.position);
+    for particle in model.particle.iter() {
+        draw.ellipse()
+            .color(DARKGREY)
+            .w_h(10.0, 10.0)
+            .xy(particle.position);
+    }
 
+    let fps_text = format!("FPS: {:.0}", app.fps());
+    draw.text(&fps_text)
+        .font_size(16)
+        .color(WHITE)
+        .x_y(win.left() + 40.0, win.top() - 20.0);
     draw.to_frame(app, &frame).unwrap();
 }
